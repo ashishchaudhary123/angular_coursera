4394ddc10a90c9b98d051705a9f56ae79fad88fc
Ashish <ashishchaudhary123@yahoo.com>
2020-09-09 15:33:44 -0400

    Components part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+  // so that this component can be imported !!
 export class AppComponent {
   title = 'conFusion';
 }
